57936a393bb00bd333bd05aebdce58eaca92e3d4
Tony Rios <awrndfw@MacBook-Pro.local>
2017-05-18 23:23:12 -0500

    updated folders

---
+++ javascript/game.js
@@ -0,0 +1,5 @@
+var randomNumber = document.getElementById("random-number");
+var myWins = document.getElementById("wins");
+var myLosses = document.getElementById("losses");
+var totalScore = document.getElelmentById("total-score");
+var images = ["images/bluesapphire.png", "images/Ruby.png", "images/Smoky_Quartz.png", "images/topaz.png"];
